e9ae6c0946f503f71bffac983ff719dc872e313c
anderarrate <158859612+anderarrate@users.noreply.github.com>
2024-05-10 19:26:17 +0200

    ex04

---
+++ ex04. Contador de vocales.py
@@ -0,0 +1,24 @@
+'''Crea un programa que cuente el número de vocales en una palabra ingresada por el usuario.'''
+
+def contar_vocales(palabra):
+  palabra = palabra.lower()
+  contador_a = contador_e = contador_i = contador_o = contador_u = 0
+  for letra in palabra:
+    if letra == 'a':
+      contador_a +=1
+    elif letra == 'e':
+      contador_e +=1
+    elif letra == 'i':
+      contador_i +=1
+    elif letra == 'o':
+      contador_o +=1
+    elif letra =='u':
+      contador_u +=1
+  total_vocales = contador_a + contador_e + contador_i + contador_o + contador_u
+  return total_vocales
+
+palabra_usuario = input('Introduce palabra: ')
+numero_vocales = contar_vocales(palabra_usuario)
+print('El número de vocales de la palabra '{}' es: '{}''.format(palabra_usuario,numero_vocales))
+
+
